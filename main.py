23072f306764cdce3fd4e8560268f2c8708ac703
Arthur Lino <arthur.ssLino@gmail.com>
2024-03-12 16:33:34 -0300

    built Scrapper as a class | started developing ui

--- main.py
+++ main.py
@@ -1,67 +1,58 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import time
+from scrapper import Scrapper, ScrapperModes
 
-locais = [
-    "Av. Paulo Prado",
-    "Av. Ver. José Francisco Damasceno",
-    "Bairro Parque Dos Estados",
-    "Bairro Terra Nobre",
-    "Cond. Santa Isabel",
-    "Estrada Atilio Biscuola",
-    "Estrada da Boiada",
-    "Estrada Miguel Bossi",
-    "Estacionamento",
-    "Estacionamento Cemitério",
-    "Estacionamento Interno da Polícia Civil",
-    "Pátio de Balizas das Autos Escolas",
-    "Rua Angelo Steck",
-    "Rua Antonio Chicalhone",
-    "Rua Arthur de Souza Singel",
-    "Rua Catarina Calssavara",
-    "Rua das Rosas",
-    "Rua Francisco Pagotto",
-    "Rua Natal Tarallo",
-    "Rua Nerina",
-    "Rua Nicola Tarallo",
-    "Rua Roberto Mazzali",
-    "Rua Santo ScarenAe",
-    "Rua São Carlos",
-    "Rua Treze de Junho",
-    "Rua Victorio Finamore"
-]
+import pandas as pd
 
-city, state = "LOUVEIRA", "SP"
+from tkinter import ttk
+from tkinter import *
 
-driver = webdriver.Chrome()
-driver.get('https://buscacepinter.correios.com.br/app/localidade_logradouro/index.php')
 
-for l in locais:
-    try:
-        uf = driver.find_element(By.NAME, "uf")
-        uf.send_keys(state)
-        local = driver.find_element(By.NAME, 'localidade')
-        local.send_keys(city)
-        log = driver.find_element(By.NAME, 'logradouro')
-        print(f'{l.split(" ", 1)[1]}:')
-        log.send_keys(l.split(" ", 1)[1])
+def create_new_scrapper(u, c, l):
+    print("creating new scrapper...")
+    s = Scrapper(ScrapperModes.NEIGHBOURHOOD, u, c, l)
+    print("starting to scrap")
+    s.fetch()
+    print("scrapping finished")
 
-        button = driver.find_element(By.ID, 'btn_pesquisar')
-        button.send_keys(Keys.ENTER)
 
-        time.sleep(1)
+root = Tk()
+root.title("thornscrub | informações de ruas - correios")
 
-        trs = driver.find_elements(By.TAG_NAME, 'tr')
-        for tr in trs[1:-1]:
-            print([el.text for el in tr.find_elements(By.XPATH, '*')])
+mainframe = ttk.Frame(root,)
 
-        exit = driver.find_element(By.ID, 'btn_nbusca')
-        exit.send_keys(Keys.ENTER)
+mainframe.grid(column=0, row=0, columnspan=8, rowspan=8)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 
-        time.sleep(1)
+uf_label = ttk.Label(mainframe, text="UF:")
+city_label = ttk.Label(mainframe, text="Cidade:")
+locality_label = ttk.Label(mainframe, text="Bairro: ")
 
-    except:
-        print('error')
+uf_entry = ttk.Entry(mainframe)
+city_entry = ttk.Entry(mainframe)
+locality_entry = ttk.Entry(mainframe)
+
+uf_label.grid(column=1, row=1, padx=4, pady=4 )
+city_label.grid(column=3, row=1, padx=4, pady=4)
+locality_label.grid(column=1, row=2, padx=4, pady=4, sticky='w')
+
+uf_entry.grid(column=2, row=1, padx=4, pady=4)
+city_entry.grid(column=4, row=1, padx=4, pady=4)
+locality_entry.grid(column=2, columnspan=3, row=2, padx=4, pady=4)
+
+btn_search = ttk.Button(mainframe, text="Pesquisar", command=lambda: create_new_scrapper(uf_entry.get(), city_entry.get(), locality_entry.get()))
+btn_search.grid(column=2, columnspan=2, row=7, padx=4, pady=4)
+
+root.mainloop()
+
+'''addresses = s.fetch()'''
+
+"""
+df_of_addresses = pd.DataFrame.from_records(addresses)
+'''df_of_addresses.to_csv("prototype.csv", index=False)
+df_of_addresses.to_excel("prototype.xlsx")'''
+
+sorted_addresses = sorted(addresses, key=lambda d: d["sub_endereco"])
+
+df_by_sub_addresses = pd.DataFrame.from_records(sorted_addresses)
+'''df_by_sub_addresses.to_csv("prototype_s.csv", index=False)
+df_by_sub_addresses.to_excel("prototype_s.xlsx")'''"""
---
+++ scrapper.py
@@ -0,0 +1,90 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from enum import Enum
+
+import time
+
+
+class ScrapperModes(Enum):
+    LOCALITY = "locality"
+    NEIGHBOURHOOD = "neighbourhood"
+
+
+class Scrapper:
+
+    def __init__(self, mode, uf, city, locality):
+        self.driver = None
+        self.modes = {
+            "locality": "https://buscacepinter.correios.com.br/app/localidade_logradouro/index.php",
+            "neighbourhood": "https://buscacepinter.correios.com.br/app/logradouro_bairro/index.php",
+        }
+        self.current_mode, self.inputs = self.setup_scrapper_mode(mode.value)
+        self.scrap_params = dict(zip(self.inputs, [uf, city, locality]))
+
+    def setup_scrapper_mode(self, selected_mode):
+        input_ids_by_mode = {
+            "locality": ["uf", "localidade", "logradouro"],
+            "neighbourhood": ["uf", "localidade", "bairro", "javascript:pesquisarPróximo"],
+        }
+
+        return self.modes[selected_mode], input_ids_by_mode[selected_mode]
+
+    def start_driver(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.get(self.current_mode)
+
+    def fetch(self):
+        self.start_driver()
+        d = self.driver
+
+        if self.current_mode == self.modes["neighbourhood"]:
+
+            for form_inputs in self.inputs[:-1]:
+                current_input = d.find_element(By.ID, form_inputs)
+                current_input.send_keys(self.scrap_params[form_inputs])
+
+            d.find_element(By.ID, 'btn_pesquisar').send_keys(Keys.ENTER)
+
+            response = []
+            has_next_page = True
+
+            while has_next_page:
+
+                time.sleep(.5)
+
+                trs = d.find_elements(By.TAG_NAME, 'tr')
+
+                for tr in trs[1:-2]:
+
+                    sub_address = ""
+                    if len(tr.find_elements(By.XPATH, '*')[0].text.split(" ", 1)[1].split("(", 1)) > 1:
+                        sub_address = tr.find_elements(By.XPATH, '*')[0].text.split(" ", 1)[1].split("(", 1)[1][:-1]
+
+                    new_address = {
+                        "tipo_de_endereco": tr.find_elements(By.XPATH, '*')[0].text.split(" ", 1)[0],
+                        "endereco": tr.find_elements(By.XPATH, '*')[0].text.split(" ", 1)[1].split("(", 1)[0],
+                        "bairro": tr.find_elements(By.XPATH, '*')[1].text,
+                        "cep": tr.find_elements(By.XPATH, '*')[-1].text,
+                        "sub_endereco": sub_address,
+                    }
+
+                    response.append(new_address)
+                    d.implicitly_wait(10)
+                    ActionChains(d).move_to_element(tr)
+
+                '''response, can_get_more = self.get_data()
+                if can_get_more:
+                    self.get_data(data=response)'''
+
+                if d.find_element(By.XPATH, '/html/body/main/form/div[1]/div[2]/div/div[3]/div/a[2]').text == "Próximo":
+                    d.find_element(By.XPATH, '/html/body/main/form/div[1]/div[2]/div/div[3]/div/a[2]').send_keys(Keys.ENTER)
+                    has_next_page = True
+                else:
+                    has_next_page = False
+
+            return response
